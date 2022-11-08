e17d5067f1824f7d39c8139a8d08638fd3e07011
M-Rahul1 <rahul2001ramesh@gmail.com>
2022-11-08 15:27:06 +0530

    Creating Routes & Schema for Storing Notes & Users

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
